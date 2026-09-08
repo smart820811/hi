a79f28ec603a68699d64ea9d0e9d474ef8965af6
Emory Su <smart820811@gmail.com>
2026-09-08 23:21:04 +0800

    update some code

--- pw.py
+++ pw.py
@@ -1,13 +1,10 @@
 pw = 'a123456'
 count = 3
-while True:
+while count > 0:
 	entry = input('請輸入密碼')
 	if entry == pw:
 		print('登入成功')
 		break
 	else:
 		count = count - 1
-		if count > 0:
-			print('密碼錯誤！還有',count,'次機會')
-		else:
-			break
+		print('密碼錯誤！還有',count,'次機會')
