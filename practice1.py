937d502a9eea300c7ef78dd233f610da1d2bb415
Emory Su <smart820811@gmail.com>
2026-09-11 16:26:56 +0800

    add count feature

--- practice1.py
+++ practice1.py
@@ -1,8 +1,11 @@
 import random
 ans = random.randint(1,100)
 print(ans)
+count = 0
 while True:
 	qes = int(input('請輸入一個整數'))
+	count += 1
+	print('第',count,'次')
 	if ans == qes:
 		print('終於猜對了')
 		break
