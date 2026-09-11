e26f04c4c94dca341a8602f52da7b1839275484c
Emory Su <smart820811@gmail.com>
2026-09-12 01:55:42 +0800

    add calculate feature

--- loop.py
+++ loop.py
@@ -1,8 +1,10 @@
 data = []
+words = 0
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line.strip())
+		words += len(line)
 		if len(data) % 1000 == 0:
 			print(len(data))
-print(data[0])
-print(data[1])
+print('已經讀完檔案了,總共是', len(data), '筆')
+print('留言平均長度為', words/len(data))
